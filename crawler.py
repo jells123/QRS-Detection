5cea55daee28a767d7dcf930e3e20fb13712727d
Julia Bedziechowska <julka.bedziechowska@gmail.com>
2019-05-23 17:54:28 +0100

    Add crawler to download data

---
+++ crawler.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import time
+
+if not os.path.exists('data'):
+	os.mkdir('data')
+dest_folder = os.getcwd() + "/data/"
+
+url = 'https://physionet.org/pn3/incartdb/'
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html5lib')
+
+hrefs_list = soup.find_all('a', href=True)
+for entry in hrefs_list:
+	link = entry['href']
+	if link.endswith('.dat') or link.endswith('.atr') or link.endswith('.hea'):
+		print(url + link + "...")
+		time.sleep(1)
+		data_request = requests.get(url + link)
+		with open(dest_folder + link, 'wb') as f:
+			f.write(data_request.content)
+		print(link, data_request.status_code)
+		
